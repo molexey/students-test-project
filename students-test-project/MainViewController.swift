049c145df85e683a20b3c230b53c48feeb2e352c
Alexey Moskalenko <a.moskalenko@appricot.ru>
2023-02-08 12:54:12 +0300

    Добавлен скролл (картинка уезжает вместе с контентом, кнопка прибита к низу экрана)

--- students-test-project/MainViewController.swift
+++ students-test-project/MainViewController.swift
@@ -11,6 +11,15 @@ class MainViewController: UIViewController {
     
     // MARK: - Views
     
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.showsVerticalScrollIndicator = false
+        view.isScrollEnabled = true
+        view.alwaysBounceVertical = false
+        view.bounces = false
+        return view
+    }()
+    
     private lazy var contentView: UIView = {
         let view = UIView()
         view.backgroundColor = .clear
@@ -90,26 +99,39 @@ class MainViewController: UIViewController {
         showAlert("Поздравляем!", andAlertMessage: "Ваша заявка успешно отправлена!", andButtonTitle: "Закрыть")
     }
     
+    // MARK: - Private methods
+    
     private func setupUI() {
-        view.addSubview(contentView)
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        
+        scrollView.addSubview(contentView)
         contentView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
-        contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
-        contentView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
-        contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 0).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 0).isActive = true
+        contentView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: 0).isActive = true
         
         contentView.addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 500).isActive = true
         imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0).isActive = true
         imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0).isActive = true
         
         contentView.addSubview(internshipView)
         internshipView.translatesAutoresizingMaskIntoConstraints = false
-        internshipView.heightAnchor.constraint(equalToConstant: 276).isActive = true
-        internshipView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        internshipView.heightAnchor.constraint(equalToConstant: 600).isActive = true
+        internshipView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -20).isActive = true
+        internshipView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
         internshipView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0).isActive = true
         internshipView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0).isActive = true
+        
         internshipView.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.topAnchor.constraint(equalTo: internshipView.topAnchor, constant: 24).isActive = true
@@ -117,7 +139,7 @@ class MainViewController: UIViewController {
         stackView.trailingAnchor.constraint(equalTo: internshipView.trailingAnchor, constant: -20).isActive = true
         stackView.addArrangedSubview(titleLabel)
         stackView.addArrangedSubview(subtitleLabel)
-//        stackView.addArrangedSubview(courseChipsView)
+
         internshipView.addSubview(choiceSpecializationView)
         choiceSpecializationView.translatesAutoresizingMaskIntoConstraints = false
         choiceSpecializationView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 12).isActive = true
@@ -126,7 +148,7 @@ class MainViewController: UIViewController {
         
         internshipView.addSubview(sendButton)
         sendButton.translatesAutoresizingMaskIntoConstraints = false
-        sendButton.topAnchor.constraint(equalTo: choiceSpecializationView.bottomAnchor, constant: 32).isActive = true
+        sendButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
         sendButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
         sendButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         sendButton.widthAnchor.constraint(equalToConstant: 219).isActive = true
@@ -135,7 +157,7 @@ class MainViewController: UIViewController {
         joinUsLabel.translatesAutoresizingMaskIntoConstraints = false
         joinUsLabel.centerYAnchor.constraint(equalTo: sendButton.centerYAnchor).isActive = true
         joinUsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        joinUsLabel.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: 20).isActive = true
+        joinUsLabel.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: -20).isActive = true
     }
     
     public func showAlert(_ alertTitle: String? = nil, andAlertMessage alertMessage: String? = nil, andButtonTitle buttonTitle: String? = "Ok", completion: ((UIAlertAction) -> Void)? = nil) {
